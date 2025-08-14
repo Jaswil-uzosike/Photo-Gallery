2e15c002471c5356cc48d7c376ffdfd18170488c
Jaswil-uzosike <izunnauzosike@gmail.com>
2025-08-14 13:36:49 +0100

    Gallery Controller and index page

---
+++ photo-gallery/PhotoGallery.Web/Controllers/GalleriesController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhotoGallery.Web.Data;
+using PhotoGallery.Web.Models;
+
+namespace PhotoGallery.Web.Controllers
+{
+    [AllowAnonymous]
+    public class GalleriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public GalleriesController(ApplicationDbContext db) => _db = db;
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _db.Galleries
+                .OrderByDescending(g => g.CreatedUtc)
+                .Take(50) //let me test small for now
+                .ToListAsync();
+
+            return View(model);
+        }
+    }
+}
